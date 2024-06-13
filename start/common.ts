c6199d83fe8d4c74c59443a09f804d5ddd203fe2
nikhil.b_wac_node <nikhil.b@webandcrafts.com>
2024-06-13 16:17:19 +0530

    removed common

--- start/common.ts
+++
@@ -1,6 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-    Route.post('/user-registration', 'AuthenticationController.registerUser')
-    Route.post('/user-login', 'AuthenticationController.tryLogin')
-  }).prefix('/api')
